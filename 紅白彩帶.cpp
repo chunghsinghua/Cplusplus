53cc324bebe6a42a3f41e7238be8a85ebdc09bc7
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-01-17 10:51:51 +0800

    Update 紅白彩帶.cpp

--- 紅白彩帶.cpp
+++ 紅白彩帶.cpp
@@ -5,67 +5,62 @@ using namespace std;
 #define N 100010
 typedef long long ll;
 
-int c[N], p[N];
+int d[N], p[N];
+
 multiset<int> mset;
 
-int _find(int x)
+int _find(int a)
 {
-    if (p[x] < 0)
-        return x;
-    return p[x] = _find(p[x]);
+    if (p[a] < 0)
+        return a;
+    return p[a] = _find(p[a]);
 }
-void _union(int x, int y)
+
+void _union(int a, int b)
 {
-    int g1 = _find(x);
-    int g2 = _find(y);
-    if (-p[g1] < -p[g2])
-        swap(g1, g2);
+    int g1 = _find(a);
     auto it = mset.find(-p[g1]);
     mset.erase(it);
+    int g2 = _find(b);
     it = mset.find(-p[g2]);
     mset.erase(it);
-    if (g1 != g2)
-    {
-        p[g1] += p[g2];
-        p[g2] = g1;
-    }
+    p[g1] += p[g2];
+    p[g2] = g1;
     mset.insert(-p[g1]);
 }
 
 int main()
 {
-    freopen("Q_7_11_1.in", "r", stdin);
-    memset(p, -1, sizeof(p));
+    // freopen("P_3_10_5.in", "r", stdin);
     int n, k;
     cin >> n >> k;
     for (int i = 1; i <= n; ++i)
     {
-        cin >> c[i];
-        if (c[i])
+        cin >> d[i];
+        if (d[i])
         {
             p[i] = -1;
             mset.insert(1);
-            if (c[i - 1])
+            if (d[i - 1])
                 _union(i - 1, i);
         }
     }
-    int mx = *mset.rbegin();
-    int mn = *mset.begin();
+    int mx = *mset.rbegin(), mn = *mset.begin();
+    int t;
     for (int i = 0; i < k; ++i)
     {
-        int idx;
-        cin >> idx;
-        c[idx] = 1;
-        p[idx] = -1;
+        cin >> t;
+        d[t] = 1;
+        p[t] = -1;
         mset.insert(1);
-        if (c[idx - 1])
-            _union(idx - 1, idx);
-        if (c[idx + 1])
-            _union(idx, idx + 1);
+        if (d[t - 1])
+            _union(t - 1, t);
+        if (d[t + 1])
+            _union(t, t + 1);
         mx += *mset.rbegin();
         mn += *mset.begin();
     }
     cout << mx << "\n"
-         << mn << "\n";
+         << mn;
     return 0;
 }
