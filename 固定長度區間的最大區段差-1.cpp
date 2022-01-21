b324b7c7dc69a6a8e424384f2095f5a23281c3c8
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-01-21 10:12:56 +0800

    Update 固定長度區間的最大區段差-1.cpp

--- 固定長度區間的最大區段差-1.cpp
+++ 固定長度區間的最大區段差-1.cpp
@@ -1,28 +1,26 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define N 200010
 typedef long long ll;
 
 int d[N];
-multiset<int> mset;
 
 int main()
 {
-    freopen("P_3_8_3.in", "r", stdin);
+    freopen("P_3_8_5.in", "r", stdin);
     int n, l;
     cin >> n >> l;
     for (int i = 0; i < n; ++i)
         cin >> d[i];
+    multiset<int> mset;
     for (int i = 0; i < l; ++i)
         mset.insert(d[i]);
     int ans = *mset.rbegin() - *mset.begin();
-    for (int i = l; i < n; ++i)
+    for (int i = l, left = 0; i < n; ++i, ++left)
     {
-        auto it = mset.find(d[i - l]);
-        mset.erase(it);
         mset.insert(d[i]);
+        mset.erase(d[left]);
         ans = max(ans, *mset.rbegin() - *mset.begin());
     }
     cout << ans;
