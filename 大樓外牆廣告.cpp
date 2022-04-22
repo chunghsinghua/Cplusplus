19ca2991255ea9573be2f0434b3274b766cf8415
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-04-22 08:45:04 +0800

    Update 大樓外牆廣告.cpp

--- 大樓外牆廣告.cpp
+++ 大樓外牆廣告.cpp
@@ -2,36 +2,36 @@
 
 using namespace std;
 
-#define N 100005
+#define N 100010
 typedef long long ll;
 
-int d[N];
+ll d[N];
 
 int main()
 {
     freopen("P_5_7_5.in", "r", stdin);
     int n;
     cin >> n;
-    d[0] = d[n + 1] = 1;
     for (int i = 1; i <= n; ++i)
         cin >> d[i];
-
-    stack<int> stk;
-    stk.push(0);
+    d[0] = d[n + 1] = -1;
+    stack<ll> s;
+    s.push(0);
     ll mx = 0;
     for (int i = 1; i <= n + 1; ++i)
     {
-        if (d[i] > d[stk.top()])
-            stk.push(i);
-        if (d[i] >= d[stk.top()])
+        if (d[i] > d[s.top()])
+            s.push(i);
+        if (d[i] == d[s.top()])
             continue;
-        while (d[i] < d[stk.top()])
+        while (d[i] < d[s.top()])
         {
-            ll height = d[stk.top()];
-            stk.pop();
-            mx = max(mx, (i - stk.top() - 1) * height);
+            ll height = d[s.top()];
+            s.pop();
+            mx = max(mx, height * (i - s.top() - 1));
         }
-        stk.push(i);
+        if (d[i] > d[s.top()])
+            s.push(i);
     }
     cout << mx;
     return 0;
