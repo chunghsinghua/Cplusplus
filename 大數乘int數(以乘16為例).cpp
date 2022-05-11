1a5fb46ff45f40935d9e9bbfcb259d7ad8881401
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-05-11 09:46:06 +0800

    Update and rename 大數乘int數(以乘4為例).cpp to 大數乘int數(以乘16為例).cpp

---
+++ 大數乘int數(以乘16為例).cpp
@@ -0,0 +1,10 @@
+for (int i = 0; i < n; ++i)
+{
+    int carry = 0;
+    for (int j = 0; j < N; ++j)
+    {
+        d[j] = d[j] * 16 + carry;
+        carry = d[j] / 10;
+        d[j] %= 10;
+    }
+}
