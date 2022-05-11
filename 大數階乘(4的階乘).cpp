c48cb474164dfc9b85bf2c646938c435f5d12368
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-05-11 09:45:25 +0800

    Update 大數階乘(4的階乘).cpp

--- 大數階乘(4的階乘).cpp
+++ 大數階乘(4的階乘).cpp
@@ -4,12 +4,12 @@ void power(int n)
 {
     for (int i = 0; i < n; ++i)
     {
+        int carry = 0;
         for (int j = 0; j < N; ++j)
-            d[j] *= 4;
-    }
-    for (int i = 0; i < N - 1; ++i)
-    {
-        d[i + 1] += d[i] / 10;
-        d[i] %= 10;
+        {
+            d[j] = d[j] * 16 + carry;
+            carry = d[j] / 10;
+            d[j] %= 10;
+        }
     }
 }
