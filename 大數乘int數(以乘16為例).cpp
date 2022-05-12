1c5da2dd1eb421f15e5bc41d40940472dd512a4a
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-05-12 10:52:44 +0800

    Update 大數乘int數(以乘16為例).cpp

--- 大數乘int數(以乘16為例).cpp
+++ 大數乘int數(以乘16為例).cpp
@@ -1,10 +1,7 @@
-for (int i = 0; i < n; ++i)
+int carry = 0;
+for (int j = 0; j < N; ++j)
 {
-    int carry = 0;
-    for (int j = 0; j < N; ++j)
-    {
-        d[j] = d[j] * 16 + carry;
-        carry = d[j] / 10;
-        d[j] %= 10;
-    }
+    d[j] = d[j] * 16 + carry;
+    carry = d[j] / 10;
+    d[j] %= 10;
 }
