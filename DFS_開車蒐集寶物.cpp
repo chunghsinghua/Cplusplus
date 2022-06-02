762e8faafeb82036eb06e37d1c9ec1ad8f74c256
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-06-02 14:02:51 +0800

    Create DFS_開車蒐集寶物.cpp

---
+++ DFS_開車蒐集寶物.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 50010
+typedef long long ll;
+
+int d[N];
+bool visit[N];
+vector<int> adj[N];
+
+int dfs(int n)
+{
+    visit[n] = true;
+    int ret = d[n];
+    for (int e : adj[n])
+    {
+        if (!visit[e])
+        {
+            visit[e] = true;
+            ret += dfs(e);
+        }
+    }
+    return ret;
+}
+
+int main()
+{
+    freopen("P_7_2_5.in", "r", stdin);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+        cin >> d[i];
+    int from, to;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> from >> to;
+        adj[from].push_back(to);
+        adj[to].push_back(from);
+    }
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+        if (!visit[i])
+            ans = max(ans, dfs(i));
+
+    cout << ans;
+
+    return 0;
+}
