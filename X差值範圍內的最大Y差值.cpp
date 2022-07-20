d00c9c5bd0053a64d1a4d2f0692293db2e1000a8
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-07-20 10:39:41 +0800

    Update X差值範圍內的最大Y差值.cpp

--- X差值範圍內的最大Y差值.cpp
+++ X差值範圍內的最大Y差值.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define N 200010
 typedef long long ll;
 
-pair<int, int> d[N];
+pair<int, int> p[N];
 
 int main()
 {
-    freopen("Q_3_13_2.in", "r", stdin);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    freopen("Q_3_13_5.in", "r", stdin);
     int n, l;
     cin >> n >> l;
     for (int i = 0; i < n; ++i)
-        cin >> d[i].first;
+        cin >> p[i].first;
     for (int i = 0; i < n; ++i)
-        cin >> d[i].second;
-    sort(d, d + n);
-    int ans = -1;
+        cin >> p[i].second;
+
+    sort(p, p + n);
     multiset<int> mset;
-    mset.insert(d[0].second);
-    for (int left = 0, right = 1; right < n; ++right)
+    int mx = 0;
+
+    for (int left = 0, right = 0; right < n; ++right)
     {
-        mset.insert(d[right].second);
-        while (d[right].first - d[left].first > l)
+        mset.insert(p[right].second);
+        while (p[right].first - p[left].first > l)
         {
-            mset.erase(d[left].second);
+            mset.erase(p[left].second);
             left++;
         }
-        ans = max(ans, *mset.rbegin() - *mset.begin());
+        mx = max(mx, abs(*mset.rbegin() - *mset.begin()));
     }
-    cout << ans;
 
+    cout << mx;
     return 0;
 }
