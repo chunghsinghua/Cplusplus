ed985ea033d9c4d8ff0fcdc32195c3675eff1bdb
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-09-03 13:08:29 +0800

    Update APCS201810_子集合的和(recursive).cpp

--- APCS201810_子集合的和(recursive).cpp
+++ APCS201810_子集合的和(recursive).cpp
@@ -2,30 +2,33 @@
 
 using namespace std;
 
-#define N 50010
+#define N 210
 typedef long long ll;
 
-int d[N], p = 10009, n, ans = 0;
-void fun(int idx, ll prod)
+int d[N], ans = 0;
+int n, p;
+
+void fun(int idx, int sum)
 {
     if (idx >= n)
     {
-        if (prod == 1)
-            ans++;
+        if (sum <= p)
+        {
+            ans = max(ans, sum);
+        }
         return;
     }
-    fun(idx + 1, prod * d[idx] % p);
-    fun(idx + 1, prod);
+    fun(idx + 1, sum + d[idx]);
+    fun(idx + 1, sum);
 }
 
 int main()
 {
-    freopen("p_1_7_5.in", "r", stdin);
-    cin >> n;
+    freopen("q_1_8_3.in", "r", stdin);
+    cin >> n >> p;
     for (int i = 0; i < n; ++i)
         cin >> d[i];
-
-    fun(0, 1);
-    cout << ans - 1;
+    fun(0, 0);
+    cout << ans;
     return 0;
 }
