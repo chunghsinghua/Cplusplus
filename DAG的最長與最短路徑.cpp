64c761d2bb207b8c544b4b405f397c5b650f4f87
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-11-10 11:08:45 +0800

    Update DAG的最長與最短路徑.cpp

--- DAG的最長與最短路徑.cpp
+++ DAG的最長與最短路徑.cpp
@@ -11,52 +11,49 @@ int pmn[N], pmx[N], indeg[N];
 
 int main()
 {
-    freopen("P_7_6_5.in", "r", stdin);
+    // freopen("P_7_6_2.in", "r", stdin);
     int n, m, s, t;
     cin >> n >> m >> s >> t;
     for (int i = 0; i < m; ++i)
     {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({v, w});
-        indeg[v]++;
+        int from, to, w;
+        cin >> from >> to >> w;
+        adj[from].push_back({to, w});
+        indeg[to]++;
     }
-    for (int i = 0; i < n; ++i)
-    {
-        pmn[i] = oo;
-        pmx[i] = -oo;
-    }
-    pmn[s] = pmx[s] = 0;
 
+    for (int i = 0; i < n; ++i)
+        pmn[i] = oo, pmx[i] = -oo;
+    pmn[s] = pmx[s] = 0;
     queue<int> que;
-    int cnt = 0;
     for (int i = 0; i < n; ++i)
         if (indeg[i] == 0)
             que.push(i);
+
+    int cnt = 0;
     while (!que.empty())
     {
         int v = que.front();
-        que.pop(), cnt++;
-        for (auto u : adj[v])
+        que.pop();
+        cnt++;
+        for (auto e : adj[v])
         {
             if (pmn[v] < oo)
             {
-                pmn[u.first] = min(pmn[u.first], pmn[v] + u.second);
-                pmx[u.first] = max(pmx[u.first], pmx[v] + u.second);
+                pmn[e.first] = min(pmn[e.first], pmn[v] + e.second);
+                pmx[e.first] = max(pmx[e.first], pmx[v] + e.second);
             }
-            if (--indeg[u.first] == 0)
-                que.push(u.first);
+            if (--indeg[e.first] == 0)
+                que.push(e.first);
         }
     }
+
     if (cnt != n)
-        cout << "not a dag\n";
-    if (pmn[t] == oo)
+        cout << "Not a DAG/n";
+    if (pmn[t] >= oo)
         cout << "No path\nNo path";
     else
-    {
         cout << pmn[t] << '\n'
              << pmx[t];
-    }
-
     return 0;
 }
