875cab6367f07c9c7a9ae375b9d10a0d2e72d6e4
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-11-20 21:13:47 +0800

    Update APCS202010_人口遷移.py

--- APCS202010_人口遷移.py
+++ APCS202010_人口遷移.py
@@ -3,25 +3,23 @@ r += 2
 c += 2
 mat = [[] for _ in range(r)]
 mat[0] = mat[r-1] = [-1 for _ in range(c)]
+temp = [[-1]*c for _ in range(r)]
 for i in range(1, r-1):
     mat[i] = [-1]+[int(x) for x in input().split()]+[-1]
-temp = [[0]*c for _ in range(r)]
 
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 for day in range(m):
-    for i in range(r):
-        for j in range(c):
+    for i in range(1, r-1):
+        for j in range(1, c-1):
             temp[i][j] = mat[i][j]
             if mat[i][j] >= 0:
                 for dir in range(4):
-                    ni = i+di[dir]
-                    nj = j+dj[dir]
+                    ni, nj = i+di[dir], j+dj[dir]
                     if mat[ni][nj] >= 0:
                         temp[i][j] -= mat[i][j]//k
                         temp[i][j] += mat[ni][nj]//k
-
     mat, temp = temp, mat
 
-print(min(mat[i][j] for i in range(r) for j in range(c) if mat[i][j] >= 0))
-print(max(mat[i][j] for i in range(r) for j in range(c)))
+print(min(mat[i][j] for i in range(r) for j in range(c) if mat[i][j] != -1))
+print(max(mat[i][j] for i in range(r) for j in range(c) if mat[i][j] != -1))
