f914b061af87be726a6908b7a69a46d11161c32d
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-12-14 09:42:39 +0800

    Create Algorithm_JumpSearch.cpp

---
+++ Algorithm_JumpSearch.cpp
@@ -0,0 +1,12 @@
+int jumpsearch(int a[], int n, int x)
+{
+    if (a[0] >= x)
+        return 0;
+  
+    int pos = 0;
+    for (int jump = n / 2; jump > 0; jump >>= 1)
+        while (pos + jump < n && a[pos + jump] < x)
+            pos += jump;
+  
+    return pos + 1;
+}
