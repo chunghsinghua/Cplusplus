e90c9e91a34bd9d6297f12f70a1a685dd32cd66c
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2022-12-15 11:11:22 +0800

    Create Algorithm_BinarySearch(like lower_bound).cpp

---
+++ Algorithm_BinarySearch(like lower_bound).cpp
@@ -0,0 +1,13 @@
+// find the first >= x between a[left..right]
+int bsearch(int a[], int left, int right, int x)
+{
+    while (left < right)
+    {
+        int mid = (left + right) >> 1;
+        if (a[mid] < x)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
