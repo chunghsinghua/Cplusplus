b48aaa6ead97fa1700cf3f9c1a11eac361fb1927
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-02-06 23:12:06 +0800

    Update APCS201906_機器人的路徑.cpp

--- APCS201906_機器人的路徑.cpp
+++ APCS201906_機器人的路徑.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define N 100
+#define N 110
 typedef long long ll;
 
 int mat[N][N];
 
 int main()
 {
-    // freopen("P_6_19_3.in", "r", stdin);
-    int m, n;
+    freopen("P_7_3_5.in", "r", stdin);
+    int n, m;
     cin >> m >> n;
-    int mn = INT_MAX, si, sj;
+    memset(mat, -1, sizeof(mat));
+    int si, sj, mn = INT_MAX;
     for (int i = 1; i <= m; ++i)
-    {
         for (int j = 1; j <= n; ++j)
         {
             cin >> mat[i][j];
@@ -24,33 +23,28 @@ int main()
                 si = i, sj = j;
             }
         }
-    }
-    for (int i = 0; i <= n + 1; ++i)
-        mat[0][i] = mat[m + 1][i] = -1;
-    for (int i = 0; i <= m + 1; ++i)
-        mat[i][0] = mat[i][n + 1] = -1;
 
-    int ans = 0;
     int di[4] = {-1, 0, 1, 0}, dj[4] = {0, 1, 0, -1};
+    int total = 0;
     while (1)
     {
-        ans += mat[si][sj];
+        total += mat[si][sj];
         mat[si][sj] = -1;
-        int ni = si, nj = sj;
         mn = INT_MAX;
+        int ni, nj;
         for (int k = 0; k < 4; ++k)
         {
             int i = si + di[k], j = sj + dj[k];
-            if (mat[i][j] >= 0 && mat[i][j] < mn)
+            if (mat[i][j] != -1 && mat[i][j] < mn)
             {
-                mn = mat[i][j];
                 ni = i, nj = j;
+                mn = mat[i][j];
             }
         }
         if (mn == INT_MAX)
             break;
         si = ni, sj = nj;
     }
-    cout << ans;
+    cout << total;
     return 0;
 }
