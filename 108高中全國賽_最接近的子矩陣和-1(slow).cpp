8e1141e637b21405887eac8008f1fd8604f2430f
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-04-23 14:20:00 +0800

    Update and rename 最接近的子矩陣和 (108高中全國賽)-未完成 to 108高中全國賽_最接近的子矩陣和-1(slow).cpp

--- 108高中全國賽_最接近的子矩陣和-1(slow).cpp
+++ 108高中全國賽_最接近的子矩陣和-1(slow).cpp
@@ -2,40 +2,37 @@
 
 using namespace std;
 
-#define N 100010
+#define N 300010
 typedef long long ll;
-#define oo 1000000001
 
-int mat[60][300001];
+int mat[52][N];
 
 int main()
 {
-    freopen("Q_2_12_9.in", "r", stdin);
-    int n, m, k, t;
+    freopen("Q_2_12_2.in", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int m, n, k;
     cin >> k >> m >> n;
     for (int i = 1; i <= m; ++i)
-    {
         for (int j = 1; j <= n; ++j)
         {
-            cin >> t;
-            mat[i][j] = mat[i][j - 1] + t;
+            cin >> mat[i][j];
+            mat[i][j] += mat[i][j - 1];
         }
-    }
-    int ans = INT_MIN;
+    int ans = 0;
     for (int p = 0; p <= n; ++p)
     {
         for (int j = p + 1; j <= n; ++j)
         {
-            int s = 0;
+            int sum = 0;
             for (int i = 1; i <= m; ++i)
             {
-                s += mat[i][j] - mat[i][p];
-                if (s <= k)
-                    ans = max(ans, s);
-                if (s < 0 || s > k)
-                    s = 0;
+                sum += mat[i][j] - mat[i][p];
+                if (sum <= k)
+                    ans = max(ans, sum);
+                if (sum < 0)
+                    sum = 0;
             }
         }
     }
