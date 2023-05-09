2e2ed9ccf6a26ef98a93b448377dfb0f023b8d8c
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-05-09 11:18:16 +0800

    Update APCS201806_完全奇數-2.cpp

--- APCS201806_完全奇數-2.cpp
+++ APCS201806_完全奇數-2.cpp
@@ -1,54 +1,46 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define N 300010
+#define N 100010
 typedef long long ll;
 
 int main()
 {
-    // freopen("Q_2_12_2.in", "r", stdin);
+    // freopen("P_8_14_5.in", "r", stdin);
     string s;
     while (cin >> s)
     {
-        int len = s.size();
         string s1(s), s2(s);
+        int len = s.size();
         for (int i = 0; i < len; ++i)
         {
-            if ((s[i] - '0') % 2)
+            int num = s[i] - '0';
+            if (num & 1)
                 continue;
 
+            // s1 is big;
             s1[i] += 1;
             for (int j = i + 1; j < len; ++j)
                 s1[j] = '1';
-            //===========================
-            if (s2[i] == '0')
+
+            // s2 is small;
+            s2[i] -= 1;
+            if (s2[i] < '0')
             {
                 s2[i] = '9';
+                int j = i - 1;
+                while (j > 0 && s2[j] == '1')
+                    s2[j--] = '9';
 
-                if (s2[i - 1] == '1')
-                {
-                    int j = i - 1;
-                    while (j > 0 && s2[j] == '1')
-                    {
-                        s2[j] = '9';
-                        j--;
-                    }
-                    s2[j] -= 2;
-                    if ((s2[j] - '0') < 0)
-                        s2[j] = '0';
-                }
-                else
-                    s2[i - 1] -= 2;
+                s2[j] -= 2;
+                if (s2[j] < '0')
+                    s2[j] = '0';
             }
-            else
-                s2[i] -= 1;
             for (int j = i + 1; j < len; ++j)
                 s2[j] = '9';
 
             break;
         }
-
         cout << min(stoll(s1) - stoll(s), stoll(s) - stoll(s2)) << '\n';
     }
     return 0;
