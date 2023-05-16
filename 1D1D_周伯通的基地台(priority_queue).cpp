94465eff005f7da8c76fb364bfbf196e964ae9d6
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-05-16 10:54:49 +0800

    Update 1D1D_周伯通的基地台(priority_queue).cpp

--- 1D1D_周伯通的基地台(priority_queue).cpp
+++ 1D1D_周伯通的基地台(priority_queue).cpp
@@ -4,30 +4,30 @@ using namespace std;
 #define N 200010
 typedef long long ll;
 
-int d[N], dp[N];
+int c[N], dp[N];
 
 int main()
 {
     freopen("P_6_13_2.in", "r", stdin);
     int n, k;
     cin >> n >> k;
-    for (int i = 0; i < n; ++i)
-        cin >> d[i];
-    dp[0] = d[0];
+    for (int i = 1; i <= n; ++i)
+        cin >> c[i];
     priority_queue<pair<int, int>> pq;
-    pq.push({-dp[0], 0});
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i <= n; ++i)
     {
-        if (i <= k)
-            dp[i] = d[i];
+        while (!pq.empty() && pq.top().second < i - k * 2 - 1)
+            pq.pop();
+
+        if (i <= k + 1)
+            dp[i] = c[i];
         else
-        {
-            while (pq.top().second < i - 2 * k - 1)
-                pq.pop();
-            dp[i] = d[i] + (-pq.top().first);
-        }
+            dp[i] = c[i] + (-pq.top().first);
+
         pq.push({-dp[i], i});
     }
-    cout << *min_element(dp + n - k - 1, dp + n);
+
+    cout << *min_element(dp + n - k, dp + n + 1);
+
     return 0;
 }
