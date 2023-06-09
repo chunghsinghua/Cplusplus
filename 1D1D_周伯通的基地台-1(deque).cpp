268233a865aed8c7fe23327244ffc76bf7d6201b
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-06-09 11:35:02 +0800

    Update 1D1D_周伯通的基地台-1(deque).cpp

--- 1D1D_周伯通的基地台-1(deque).cpp
+++ 1D1D_周伯通的基地台-1(deque).cpp
@@ -11,25 +11,25 @@ int main()
     freopen("P_6_13_5.in", "r", stdin);
     int n, k;
     cin >> n >> k;
-    for (int i = 1; i <= n; ++i)
+    for (int i = 0; i < n; ++i)
         cin >> c[i];
     deque<int> deq;
-    deq.push_back(1);
-    dp[1] = c[1];
-    for (int i = 2; i <= n; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
-        if (deq.front() < i - (2 * k + 1))
-            deq.pop_front();
-        if (i <= k + 1)
+        if (i <= k)
             dp[i] = c[i];
         else
+        {
+            if (deq.front() < i - 2 * k - 1)
+                deq.pop_front();
             dp[i] = c[i] + dp[deq.front()];
-
-        while (!deq.empty() && dp[i] <= dp[deq.back()])
+        }
+        while (!deq.empty() && dp[deq.back()] >= dp[i])
             deq.pop_back();
         deq.push_back(i);
     }
-    cout << *min_element(dp + n - k, dp + n + 1);
+    cout << *min_element(dp + n - 1 - k, dp + n);
 
     return 0;
 }
