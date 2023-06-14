d4210c9c8f1975a4080b92d66aff1ff7cf9e7c8e
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-06-14 11:31:40 +0800

    Update and rename 2D1D_切棍子(top-down).cpp to 2D1D_切棍子-1(top-down).cpp

--- 2D1D_切棍子-1(top-down).cpp
+++ 2D1D_切棍子-1(top-down).cpp
@@ -7,28 +7,28 @@ typedef long long ll;
 
 int d[N], dp[N][N];
 
-int fun(int le, int ri)
+ll fun(int le, int ri)
 {
     if (dp[le][ri] >= 0)
         return dp[le][ri];
-    int mn = INT_MAX;
+    ll mn = INT_MAX;
     for (int i = le + 1; i < ri; ++i)
         mn = min(mn, fun(le, i) + fun(i, ri));
-    mn += d[ri] - d[le];
-    return dp[le][ri] = mn;
+    return dp[le][ri] = mn + d[ri] - d[le];
 }
 
 int main()
 {
-    freopen("P_6_17_5.in", "r", stdin);
+    freopen("P_6_17_1.in", "r", stdin);
     int n, l;
     cin >> n >> l;
     for (int i = 1; i <= n; ++i)
         cin >> d[i];
     d[0] = 0, d[n + 1] = l;
-    for (int i = 0; i < n + 2; ++i)
-        for (int j = i + 2; j < n + 2; ++j)
-            dp[i][j] = -1;
+    memset(dp, -1, sizeof(dp));
+    for (int i = 0; i < n + 1; ++i)
+        dp[i][i + 1] = 0;
     cout << fun(0, n + 1);
+
     return 0;
 }
