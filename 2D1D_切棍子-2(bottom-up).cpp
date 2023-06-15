05824d22519757ab64cb69080876a1adc50bb700
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-06-15 09:06:25 +0800

    Update and rename 2D1D_切棍子(bottom-up).cpp to 2D1D_切棍子-2(bottom-up).cpp

---
+++ 2D1D_切棍子-2(bottom-up).cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 210
+typedef long long ll;
+
+int d[N], dp[N][N];
+
+ll fun(int le, int ri)
+{
+    if (dp[le][ri] >= 0)
+        return dp[le][ri];
+    ll mn = INT_MAX;
+    for (int i = le + 1; i < ri; ++i)
+        mn = min(mn, fun(le, i) + fun(i, ri));
+    return dp[le][ri] = mn + d[ri] - d[le];
+}
+
+int main()
+{
+    freopen("P_6_17_5.in", "r", stdin);
+    int n, l;
+    cin >> n >> l;
+    for (int i = 1; i <= n; ++i)
+        cin >> d[i];
+    d[0] = 0, d[n + 1] = l;
+    for (int i = 0; i < n + 1; ++i)
+        dp[i][i + 1] = 0;
+    for (int len = 2; len < n + 2; ++len)
+    {
+        for (int i = 0; i + len < n + 2; ++i)
+        {
+            int j = i + len, mn = INT_MAX;
+            for (int k = i + 1; k < i + len; ++k)
+                mn = min(mn, dp[i][k] + dp[k][j]);
+            dp[i][j] = mn + d[j] - d[i];
+        }
+    }
+    cout << dp[0][n + 1];
+    return 0;
+}
