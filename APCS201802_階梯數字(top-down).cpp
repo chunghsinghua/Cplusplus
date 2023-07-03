1ee2a5b66f11359f2c9e938d9d1bb8d0147db778
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-07-03 11:22:46 +0800

    Update APCS201802_階梯數字(top-down).cpp

--- APCS201802_階梯數字(top-down).cpp
+++ APCS201802_階梯數字(top-down).cpp
@@ -6,34 +6,24 @@ using namespace std;
 typedef long long ll;
 
 int dp[N][10];
-int fun1(string &str, int len)
+
+int fun1(string &s, int len)
 {
     int i;
-    for (i = len - 2; len >= 0; --i)
-    {
-        if (str[i] < str[i + 1])
+    for (i = len - 2; i >= 0; --i)
+        if (s[i] < s[i + 1])
             break;
-    }
     if (i < 0)
         return len;
-
     for (int j = i; j >= 0; --j)
-        str[j] = 9;
+        s[j] = 9;
     int j = i + 1;
-    str[j]--;
-    while (j < len && str[j] < str[j + 1])
+    s[j] -= 1;
+    while (s.back() == 0)
     {
-        str[j] = 9;
-        j++;
-        str[j]--;
-    }
-    while (str.back() == 0)
-    {
-        str.pop_back();
+        s.pop_back();
         len--;
     }
-    // for (int k = len - 1; k >= 0; --k)
-    //     cout << (int)str[k]; //check
     return len;
 }
 
@@ -44,13 +34,12 @@ int fun2(int len, int start)
     dp[len][start] = 0;
     for (int i = start; i <= 9; ++i)
         dp[len][start] += fun2(len - 1, i);
-
     return dp[len][start];
 }
 
 int main()
 {
-    freopen("P_6_19_5.in", "r", stdin);
+    freopen("P_6_19_2.in", "r", stdin);
     string s;
     cin >> s;
     int len = s.size();
@@ -64,16 +53,17 @@ int main()
     dp[1][0] = 0;
     for (int i = 1; i <= 9; ++i)
         dp[1][i] = 1;
-    int t = fun2(len + 1, 0);
+    fun2(len + 1, 0);
 
-    int ans = 0;
-    reverse(s.begin(), s.end());
+    for (int i = 0, j = len - 1; i < j; ++i, --j)
+        swap(s[i], s[j]);
+    int cnt = 0;
     for (int i = 0; i < s[0]; ++i)
-        ans += dp[len][i];
+        cnt += dp[len][i];
     for (int i = 1; i < len; ++i)
         for (int j = s[i - 1]; j < s[i]; ++j)
-            ans += dp[len - i][j];
+            cnt += dp[len - i][j];
 
-    cout << ans + 1;
+    cout << cnt + 1;
     return 0;
 }
