6c4952239f8bbdd01f09040af8330ebde9ac01f6
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-08-06 21:12:31 +0800

    Update APCS202206_字串解碼.py

--- APCS202206_字串解碼.py
+++ APCS202206_字串解碼.py
@@ -1,30 +1,23 @@
 m, n = map(int, input().split())
 e = []
 for _ in range(m):
-    temp = input()
-    e.append(temp)
+    tmp = input()
+    e.append(tmp)
 
-t = input()
-for ss in e[::-1]:
-
-    s = ''
-    sidx = n-1
-    cnt1 = ss.count('1')
-
-    for i in ss[::-1]:
-        if i == '0':
-            s = t[sidx]+s
-            sidx -= 1
+t = list(input())
+for ee in e[::-1]:
+    s = []
+    for et in ee[::-1]:
+        if et == '1':
+            s.append(t.pop())
         else:
-            s += t[sidx]
-            sidx -= 1
+            s = [t.pop()]+s
 
-    if cnt1 % 2:
-        if (n % 2):
-            s = s[n//2+1:]+s[n//2]+s[:n//2]
+    if ee.count('1') & 1:
+        if n & 1:
+            s = s[n//2+1:]+[s[n//2]]+s[:n//2]
         else:
             s = s[n//2:]+s[:n//2]
 
-    t = s
-
-print(s)
+    t = (s)
+print("".join(t))
