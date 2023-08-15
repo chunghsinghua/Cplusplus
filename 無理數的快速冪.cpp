dde5da1778c73a0e8a748c443ff44ddb9a26a0fa
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-08-15 21:21:10 +0800

    Update 無理數的快速冪.cpp

--- 無理數的快速冪.cpp
+++ 無理數的快速冪.cpp
@@ -5,31 +5,27 @@ using namespace std;
 #define N 100010
 typedef long long ll;
 
-ll p = 1000000009;
-
-pair<ll, ll> fun(ll x, ll y, ll n)
+ll p = 1e9 + 9;
+pair<ll, ll> fun(ll x, ll y, int n)
 {
     if (n == 1)
         return {x, y};
     if (n & 1)
     {
         pair<ll, ll> t = fun(x, y, n - 1);
-        ll x2 = t.first;
-        ll y2 = t.second;
-        return {(x2 * x + 2 * y2 * y) % p, (x2 * y + y2 * x) % p};
+        ll a = t.first, b = t.second;
+        return {(a * x + 2 * b * y) % p, (b * x + a * y) % p};
     }
     pair<ll, ll> t = fun(x, y, n / 2);
-    ll x2 = t.first, y2 = t.second;
-    return {(x2 * x2 + 2 * y2 * y2) % p, 2 * x2 * y2 % p};
+    ll a = t.first, b = t.second;
+    return {(a * a + 2 * b * b) % p, 2 * a * b % p};
 }
-
 int main()
 {
     freopen("Q_2_13_5.in", "r", stdin);
-    ll x, y, n;
-    cin >> x >> y >> n;
-    pair<ll, ll> ans = fun(x, y, n);
-    cout << ans.first << ' ' << ans.second;
-
+    ll x, y, b;
+    cin >> x >> y >> b;
+    pair<ll, ll> t = fun(x, y, b);
+    cout << t.first << ' ' << t.second;
     return 0;
 }
