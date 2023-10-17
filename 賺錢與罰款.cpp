fa7c632a37dfe4cae54843723655accd77e3ac48
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2023-10-17 15:12:25 +0800

    Update 賺錢與罰款.cpp

--- 賺錢與罰款.cpp
+++ 賺錢與罰款.cpp
@@ -2,37 +2,38 @@
 
 using namespace std;
 
-#define N 100005
+#define N 100010
 typedef long long ll;
 
-struct work
+struct Work
 {
     int t, d;
-} d[N];
+} work[N];
 
-bool cmp(work a, work b)
+bool cmp(Work &a, Work &b)
 {
-    // if (a.t == b.t)
-    //     return a.d > b.d;
-    return a.t < b.t;
+    return ((a.t < b.t) || (a.t == b.t && a.d > b.d));
+    /*  if (a.t == b.t)
+         return a.d > b.d;
+     return a.t < b.t; */
 }
 
 int main()
 {
-    freopen("Q_4_16_5.in", "r", stdin);
+    freopen("Q_4_16_1.in", "r", stdin);
     int n;
     cin >> n;
     for (int i = 0; i < n; ++i)
-        cin >> d[i].t;
+        cin >> work[i].t;
     for (int i = 0; i < n; ++i)
-        cin >> d[i].d;
-    sort(d, d + n, cmp);
-    ll ans = 0, psum = 0;
+        cin >> work[i].d;
+    sort(work, work + n, cmp);
+    ll tsum = 0, total = 0;
     for (int i = 0; i < n; ++i)
     {
-        psum += d[i].t;
-        ans += d[i].d - psum;
+        tsum += work[i].t;
+        total += work[i].d - tsum;
     }
-    cout << ans;
+    cout << total;
     return 0;
 }
