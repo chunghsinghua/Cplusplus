484e4280e55eabd40378351cc747c378adab2926
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-03-17 00:03:47 +0800

    Create 108全國高中賽_寶石的顏色-2.py

---
+++ 108全國高中賽_寶石的顏色-2.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open("C:\\Users\\ChungHsingHua\\Documents\\c-program\\kevin\\P_8_7_3.in")
+
+def dfs(v):
+    global mx,cc
+    cc[c[v]]+=1
+    mx = max(mx,cc[c[v]])
+    for e in adj[v]:
+        dfs(e)
+    cc[c[v]]-=1
+
+n=int(input())
+c= [int(x) for x in input().split()]
+adj=[[] for i in range(n)]
+cc = {cidx:0 for cidx in c}
+for _ in range(n-1):
+    a,b=map(int,input().split())
+    adj[a].append(b)
+
+mx = 0
+dfs(0)
+print(mx)
