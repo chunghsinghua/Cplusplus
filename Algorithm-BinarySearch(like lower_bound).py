3dda2bb173d91e8268160dc1dd527f45ccdd7b25
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-03-31 15:27:20 +0800

    Create Algorithm-BinarySearch(like lower_bound).py

---
+++ Algorithm-BinarySearch(like lower_bound).py
@@ -0,0 +1,11 @@
+
+def fun(s,le,ri):
+    while le<=ri:
+        mid = (le+ri)//2
+        if a[mid]==s:
+            return mid
+        if a[mid]<s:
+            le = mid+1
+        else:
+            ri = mid-1
+    return le
