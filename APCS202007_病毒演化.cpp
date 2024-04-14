2cf682b95f0b207c09aace50b8111145c165ea91
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-04-14 21:27:27 +0800

    Update APCS202007_病毒演化.cpp

--- APCS202007_病毒演化.cpp
+++ APCS202007_病毒演化.cpp
@@ -1,61 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 1005
+#define N 1010
 typedef long long ll;
 
-map<char, int> conv = {{'A', 0}, {'U', 1}, {'C', 2}, {'G', 3}, {'@', 4}};
+map<char, int> cmap = {{'A', 0}, {'U', 1}, {'C', 2}, {'G', 3}, {'@', 4}};
 
 int main()
 {
-    // freopen("Q_6_5_5.in", "r", stdin);
+    // freopen("P_4_9_2.in", "r", stdin);
     int n, m, i, j;
-    string s;
     cin >> n >> m;
-    vector<vector<int>> rna(n + 1, vector<int>(m));
+    string s;
     vector<int> p(n + 1), deg(n + 1);
+    vector<vector<int>> rna(n + 1, vector<int>(m, 0));
     for (int k = 0; k < n; ++k)
     {
         cin >> i >> j >> s;
         if (i != j)
+        {
             p[i] = j;
-        deg[p[i]]++;
-        for (int j = 0; j < m; ++j)
-            rna[i][j] = conv[s[j]];
+            deg[j]++;
+        }
+        for (int k = 0; k < m; ++k)
+            rna[i][k] = cmap[s[k]];
     }
-    vector<int> dag;
-    int head = 0;
+    vector<int> que;
     for (int i = 1; i <= n; ++i)
         if (deg[i] == 0)
-            dag.push_back(i);
-    while (dag.size() < n)
+            que.push_back(i);
+    int head = 0;
+    while (head < que.size())
     {
-        int v = dag[head++];
+        int v = que[head++];
         int parent = p[v];
         if (parent == 0)
             break;
         if (--deg[parent] == 0)
-            dag.push_back(parent);
+            que.push_back(parent);
     }
     int tot = 0;
     for (int i = 0; i < m; ++i)
     {
-        int cost[n + 1][4] = {0};
-        for (int e : dag)
+        vector<vector<int>> cost(n + 1, vector<int>(4, 0));
+        for (int e : que)
         {
             int ch = rna[e][i];
             if (ch < 4)
             {
                 tot += cost[e][ch];
                 for (int j = 0; j < 4; ++j)
-                {
                     if (ch != j)
                         cost[p[e]][j]++;
-                }
             }
             else
             {
-                int mcost = 9e5;
+                int mcost = 1e9;
                 for (int j = 0; j < 4; ++j)
                     mcost = min(mcost, cost[e][j]);
                 tot += mcost;
