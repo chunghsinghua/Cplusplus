34f7f1e7bd5aebd9e81896fb004faad7cf0a8b75
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-05-19 12:44:20 +0800

    Update 最長的相異色彩帶.cpp

--- 最長的相異色彩帶.cpp
+++ 最長的相異色彩帶.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define N 200010
+#define N 510
 typedef long long ll;
-
-int c[N], cnt[N];
 
 int main()
 {
     freopen("Q_3_11_5.in", "r", stdin);
     int n;
     cin >> n;
+    vector<int> vec(n), color(n + 1);
     for (int i = 0; i < n; ++i)
-        cin >> c[i];
-    int mx = 0;
-    for (int left = 0, right = 0; right < n; ++right)
+        cin >> vec[i];
+    int cnt = 0;
+    for (int le = 0, ri = 0; ri < n; ++ri)
     {
-        int color = c[right];
-        cnt[color]++;
-        while (cnt[color] > 1)
+        color[vec[ri]]++;
+        while (color[vec[ri]] > 1)
         {
-            cnt[c[left]]--;
-            left++;
+            color[vec[le]]--;
+            le++;
         }
-        mx = max(mx, right - left + 1);
+        cnt = max(cnt, ri - le + 1);
     }
-    cout << mx;
+    cout << cnt;
     return 0;
 }
