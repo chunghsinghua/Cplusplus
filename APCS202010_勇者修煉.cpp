721bd2939c755dcc40e1a16cd71e652ab7f07b1d
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-06-10 22:32:09 +0800

    Create APCS202010_勇者修煉.cpp

---
+++ APCS202010_勇者修煉.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 50010
+typedef long long ll;
+
+int main()
+{
+    // freopen("Q_3_5_3.in", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int>> mat(m + 1, vector<int>(n)), dp(m + 1, vector<int>(n));
+    for (int i = 1; i <= m; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> mat[i][j];
+    for (int i = 1; i <= m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            dp[i][j] = dp[i - 1][j] + mat[i][j];
+            for (int k = 0; k < j; ++k)
+            {
+                int t = dp[i - 1][k];
+                for (int kk = k; kk <= j; ++kk)
+                    t += mat[i][kk];
+                dp[i][j] = max(dp[i][j], t);
+            }
+            for (int k = n - 1; k >= j; --k)
+            {
+                int t = dp[i - 1][k];
+                for (int kk = k; kk >= j; --kk)
+                    t += mat[i][kk];
+                dp[i][j] = max(dp[i][j], t);
+            }
+        }
+    }
+    cout << *max_element(dp[m].begin(), dp[m].end());
+    return 0;
+}
