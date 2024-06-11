8967e4aa54f80f492d2a6045075b2aee0b476dcf
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-06-11 13:40:30 +0800

    Update and rename APCS202010_勇者修煉.cpp to APCS202010_勇者修煉-1(slow).cpp

--- APCS202010_勇者修煉-1(slow).cpp
+++ APCS202010_勇者修煉-1(slow).cpp
@@ -7,11 +7,12 @@ typedef long long ll;
 int main()
 {
     // freopen("Q_3_5_3.in", "r", stdin);
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
     int m, n;
     cin >> m >> n;
-    vector<vector<int>> mat(m + 1, vector<int>(n)), dp(m + 1, vector<int>(n));
+    vector<vector<int>> mat(m + 1, vector<int>(n, 0));
+    vector<vector<int>> dp(m + 1, vector<int>(n, 0));
     for (int i = 1; i <= m; ++i)
         for (int j = 0; j < n; ++j)
             cin >> mat[i][j];
@@ -20,16 +21,17 @@ int main()
         for (int j = 0; j < n; ++j)
         {
             dp[i][j] = dp[i - 1][j] + mat[i][j];
+            int t;
             for (int k = 0; k < j; ++k)
             {
-                int t = dp[i - 1][k];
+                t = dp[i - 1][k];
                 for (int kk = k; kk <= j; ++kk)
                     t += mat[i][kk];
                 dp[i][j] = max(dp[i][j], t);
             }
-            for (int k = n - 1; k >= j; --k)
+            for (int k = n - 1; k > j; --k)
             {
-                int t = dp[i - 1][k];
+                t = dp[i - 1][k];
                 for (int kk = k; kk >= j; --kk)
                     t += mat[i][kk];
                 dp[i][j] = max(dp[i][j], t);
