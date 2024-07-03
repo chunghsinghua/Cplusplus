d65819a08f485d195ca39397a526cab6b6864f83
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-07-03 17:13:05 +0800

    Update APCS202406_缺字問題-2.cpp

--- APCS202406_缺字問題-2.cpp
+++ APCS202406_缺字問題-2.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #define N 50010
 typedef long long ll;
 
-void fun(string ret, string s, int t, map<string, int> &mp)
+void fun(string ret, string &s, int t, map<string, bool> &mp)
 {
     if (t == 0)
     {
-        mp.insert({ret, 0});
+        mp.insert({ret, false});
         return;
     }
     for (int i = 0; i < s.size(); ++i)
@@ -21,18 +21,19 @@ void fun(string ret, string s, int t, map<string, int> &mp)
 int main()
 {
     // freopen("P_5_4_3.in", "r", stdin);
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     string alpha, s;
-    int l;
+    int l, leng;
     cin >> alpha >> l >> s;
-    map<string, int> mp;
+    leng = s.size();
+    map<string, bool> mp;
     fun("", alpha, l, mp);
-    int leng = s.size();
-    for (int i = 0; i < leng - (l - 1); ++i)
+    string t;
+    for (int i = 0; i <= leng - l; ++i)
     {
-        string t = s.substr(i, l);
-        mp[t]++;
+        t = s.substr(i, l);
+        mp[t] = true;
     }
     for (auto it = mp.begin(); it != mp.end(); ++it)
         if (!it->second)
