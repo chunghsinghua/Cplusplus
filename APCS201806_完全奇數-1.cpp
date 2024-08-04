fa1079ca7853a17273740d0bc6ad4b8ee7f745e2
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-08-04 20:53:42 +0800

    Update APCS201806_完全奇數-1.cpp

--- APCS201806_完全奇數-1.cpp
+++ APCS201806_完全奇數-1.cpp
@@ -16,42 +16,35 @@ int main()
         for (int i = 0; i < len; ++i)
         {
             int x = in[i] - '0';
-            if (x % 2 == 0)
+            if (x % 2)
+                continue;
+
+            bin[i] += 1;
+            for (int j = i + 1; j < len; ++j)
+                bin[j] = '1';
+            //=================
+
+            if (in[i] == '0')
             {
-                bin[i] += 1;
+                int idx = i - 1;
+                while (idx > 0 && sin[idx] == '1')
+                {
+                    sin[idx] = '9';
+                    idx--;
+                }
+                sin[idx] -= 2;
+                if (sin[idx] < '0')
+                    sin[idx] = '0';
+                for (int j = i; j < len; ++j)
+                    sin[j] = '9';
+            }
+            else
+            {
+                sin[i] -= 1;
                 for (int j = i + 1; j < len; ++j)
-                    bin[j] = '1';
-                break;
+                    sin[j] = '9';
             }
-        }
-        for (int i = 0; i < len; ++i)
-        {
-            int x = in[i] - '0';
-            if (x % 2 == 0)
-            {
-                if (in[i] == '0')
-                {
-                    int idx = i - 1;
-                    while (idx > 0 && sin[idx] == '1')
-                    {
-                        sin[idx] = '9';
-                        idx--;
-                    }
-                    sin[idx] -= 2;
-                    if (sin[idx] < '0')
-                        sin[idx] = '0';
-                    for (int j = i; j < len; ++j)
-                        sin[j] = '9';
-                    break;
-                }
-                else
-                {
-                    sin[i] -= 1;
-                    for (int j = i + 1; j < len; ++j)
-                        sin[j] = '9';
-                    break;
-                }
-            }
+            break;
         }
         cout << min(stoll(in) - stoll(sin), stoll(bin) - stoll(in)) << "\n";
     }
