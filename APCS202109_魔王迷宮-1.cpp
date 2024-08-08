a6f04ba899e70a558f1f3e0603e63b6b9a331066
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-08-08 12:06:24 +0800

    Update APCS202109_魔王迷宮-1.cpp

--- APCS202109_魔王迷宮-1.cpp
+++ APCS202109_魔王迷宮-1.cpp
@@ -2,67 +2,56 @@
 
 using namespace std;
 
-#define N 110
+#define N 50010
 typedef long long ll;
 
-int mat[N][N];
+struct Monster
+{
+    int r, c, s, t;
+};
 
 int main()
 {
-    // freopen("P_8_2_5.in", "r", stdin);
-    int m, n, k;
+    // freopen("p_1_1_5.in", "r", stdin);
+    int m, n, k, r, c, s;
     cin >> m >> n >> k;
-    vector<vector<int>> gost;
+    vector<vector<int>> bmat(m, vector<int>(n, 0));
+    vector<Monster> monster(k), monster2;
     for (int i = 0; i < k; ++i)
     {
-        vector<int> instance;
-        int t;
-        for (int j = 0; j < 4; ++j)
-        {
-            cin >> t;
-            instance.push_back(t);
-        }
-        gost.push_back(instance);
+        cin >> monster[i].r >> monster[i].c >> monster[i].s >> monster[i].t;
     }
 
-    while (gost.size() > 0)
+    while (!monster.empty())
     {
-        vector<vector<int>> temp;
-        for (auto e : gost)
+        monster2.clear();
+        for (auto e : monster)
         {
-            int si = e[0], sj = e[1];
-            if (mat[si][sj])
-                mat[si][sj] = -1;
-            else
-                temp.push_back(e);
-        }
-        gost.swap(temp);
+            bmat[e.r][e.c] = 1;
+            int nr = e.r + e.s, nc = e.c + e.t;
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n)
+                continue;
 
+            if (bmat[nr][nc])
+            {
+                bmat[nr][nc] = -1;
+                continue;
+            }
+
+            e.r = nr, e.c = nc;
+            monster2.push_back(e);
+        }
         for (int i = 0; i < m; ++i)
             for (int j = 0; j < n; ++j)
-                if (mat[i][j] == -1)
-                    mat[i][j] = 0;
-
-        temp.clear();
-        for (auto e : gost)
-        {
-            int si = e[0], sj = e[1];
-            mat[si][sj] = 1;
-            int ni = si + e[2], nj = sj + e[3];
-            if (((ni >= 0) && (ni <= m)) && ((nj >= 0) && (nj <= n)))
-            {
-                e[0] = ni, e[1] = nj;
-                temp.push_back(e);
-            }
-        }
-        gost.swap(temp);
+                if (bmat[i][j] == -1)
+                    bmat[i][j] = 0;
+        monster.swap(monster2);
     }
-
     int cnt = 0;
     for (int i = 0; i < m; ++i)
         for (int j = 0; j < n; ++j)
-            cnt += mat[i][j];
-
+            if (bmat[i][j])
+                cnt++;
     cout << cnt;
     return 0;
 }
