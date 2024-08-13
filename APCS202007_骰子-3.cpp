36a8329742b8f1b469efd5d1bef4d8bc255c3d5a
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2024-08-13 19:04:22 +0800

    Create APCS202007_骰子-3.cpp

---
+++ APCS202007_骰子-3.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 50010
+typedef long long ll;
+
+struct G
+{
+    int f, back, t, bottom, r, l;
+};
+
+int main()
+{
+    // freopen("p_1_1_5.in", "r", stdin);
+    int n, m, a, b, t;
+    cin >> n >> m;
+    vector<G> game(n + 1);
+    for (int i = 1; i <= n; ++i)
+    {
+        game[i].t = 1, game[i].bottom = 6;
+        game[i].f = 4, game[i].back = 3;
+        game[i].r = 2, game[i].l = 5;
+    }
+    while (m--)
+    {
+        cin >> a >> b;
+        if (a > 0 && b > 0)
+            swap(game[a], game[b]);
+        else if (b == -1)
+        {
+            game[a].bottom = game[a].f;
+            game[a].f = game[a].t;
+            game[a].back = 7 - game[a].f;
+            game[a].t = 7 - game[a].bottom;
+        }
+        else if (b == -2)
+        {
+            game[a].bottom = game[a].r;
+            game[a].r = game[a].t;
+            game[a].t = 7 - game[a].bottom;
+            game[a].l = 7 - game[a].r;
+        }
+    }
+    for (int i = 1; i <= n; ++i)
+        cout << game[i].t << ' ';
+    return 0;
+}
